83f0e22a346d8b30ad5819d3ff04b178c5149f8b
Tom Renard <tom.renard@zoho.com>
2020-11-01 10:45:38 +0100

    custom video done

--- 11 - Custom Video Player/scripts-FINISHED.js
+++ 11 - Custom Video Player/scripts-FINISHED.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const screen = player.querySelector('.Toggle_screen');
 
 /* Build out functions */
 function togglePlay() {
@@ -37,12 +38,16 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function fullScreen() {
+  video.classList.toggle('fully');
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
-
+screen.addEventListener('click', fullScreen);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
